6cdaefe829a2d556ca36a4a85a4ac79a7fc74910
Joycie Yu <joycie@uw.edu>
2017-10-10 23:13:33 -0700

    fix operations

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -10,58 +10,58 @@ import Foundation
 
 
 print("Enter an expression separated by returns:")
+var result = 0.0
+var firstRes = readLine(strippingNewline: true)!
+let resArr = firstRes.split(separator:" ")
+let length = resArr.count
+switch length {
+case 1 :
+    let num1 = Double(firstRes)
+    let operation: String = readLine(strippingNewline: true)!
+    let secondRes = readLine(strippingNewline: true)!
+    let num2 = Double(secondRes)
 
-var response = readLine(strippingNewline: true)!
-var num = Int(response)
-var numbers : [Int] =  []
-var result = 0
-
-while (num != nil) {
-    numbers.append(num)
-    response = readLine(StrippingNewLine: true)!
-    num = Int(response)
-}
-
-
-switch response {
-case "count":
-    result = numbers.count
+    switch operation {
+    case "+" :
+        result = num1! + num2!
+    case "-" :
+        result = num1! - num2!
+    case "/" :
+        result = num1! / num2!
+    case "*" :
+        result = num1! * num2!
+    case "%" :
+        result = num1!.truncatingRemainder(dividingBy: num2!)
+    default:
+        print ("Sorry, cannot compute")
+    }
+default:
+    let operation = resArr.last!
+    switch operation {
+    case "count":
+        result = Double(resArr.count - 1)
     
-case "avg":
-    result = (numbers.reduce(0, combine:+)) / numbers.count
-    
-case "fact":
-    if (numbers.count == 1) {
-        var factSum = 1
-        for var i : int = 1; i <= numbers.first; i += 1 {
-            factSum = factSum * i;
+    case "avg":
+        for index in 0...(resArr.count - 2){
+            result += Double(resArr[index])!
+        }
+        result /= Double(resArr.count - 1)
+    case "fact":
+            var factSum = 1.0
+        let stopVal : Int = Int(resArr[0])!
+        if (stopVal > 0) {
+            for index in 1...stopVal {
+                factSum = factSum * Double(index)
+            }
+        } else {
+            for index in stopVal...(-1) {
+                factSum = factSum * Double(index)
+            }
         }
         result = factSum;
+        
+    default :
+        print ("Sorry, cannot compute")
     }
-case "%":
-    var lastLine = readLine()!
-    var lastNum = Int(lastLine) !
-        result = (numbers[0]).truncatingRemainder(dividingBy: numbers.last)!
-    
-case "+":
-    var lastLine = readLine()
-    var lastNum = Int(lastLine)
-    result = numbers[0] + lastNum
-    
-case "-":
-    var lastLine = readLine()!
-    var lastNum = Int(lastLine)
-    result = numbers[0] - lastNum
-    
-case "/":
-    var lastLine = readLine()!
-    var lastNum = Int(lastLine)
-    result = (numbers[0] / lastNum)
-    
-default:
-    result = numbers[0]
-    
 }
-
 print("Result: \(result)")
-
